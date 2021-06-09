88f8e2943628c43aa24b7cc1a3563f8d9758fb54
Yaroslav Goloborodko <y.holoborodko@m-games-ltd.com>
2021-06-09 23:59:48 +0300

    #module2-task1 fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 function randomInteger(min, max) {
   if (min < 0 || max < 0) {
-    return console.log('Значение не может быть ниже 0');
+    return;
   }
   if (min === max ) {
-    return console.log('Значение "от" не может быть равным "до"');
+    return ;
   }
   if (min > max) {
     const rand2 = max + Math.random() * (min + 1 - max);
@@ -14,16 +14,14 @@ function randomInteger(min, max) {
   return Math.floor(rand);
 }
 
-
-console.log(randomInteger(1, 5));
-
+randomInteger(1, 5);
 
 function randomFloat(min, max, float) {
   if (min < 0 || max < 0) {
-    return console.log('Значение не может быть ниже 0');
+    return;
   }
   if (min === max ) {
-    return console.log('Значение "от" не может быть равным "до"');
+    return;
   }
   if (min > max) {
     const rand2 = Math.random() * (min - max) + max;
@@ -33,5 +31,4 @@ function randomFloat(min, max, float) {
   return +rand.toFixed(float);
 }
 
-
-console.log(randomFloat(5, 1, 3));
+randomFloat(5, 1, 3);
